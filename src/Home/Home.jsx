941473eb86e8b654d572500cd5efd7266a68cf11
chanzihang <chanzihang0311@gmail.com>
2025-07-03 15:22:58 +0800

    2025-07-03 update:website degisn

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -29,22 +29,22 @@ function Home() {
     <div className="animate__animated animate__fadeIn animate__faster d-flex justify-content-center align-items-center" style={{height: '100%', overflowY:'auto',overflowX:'hidden'}}>
       {contextHolder}
       <Paragraph style={{padding:`${viewPadding}px`}}>
-        <div style={{padding:'60px'}}>
-          <div className="text-muted" style={{textJustify:'justify',fontSize:'clamp(15px, 4vw, 200px)'}}>
+        <div style={{padding:'30px'}}>
+          <div className="text-muted" style={{textJustify:'justify',fontSize:'clamp(20px, 4vw, 200px)'}}>
             CHAN ZI HANG
           </div>
-          <Row gutter={12}>
+          <Row className="my-3" gutter={12}>
             <Col>
-              <Button className="border-0 menu-btn m-1" icon={<GithubOutlined />} onClick={()=>window.open('https://github.com/enter1130')} >Github</Button>
+              <Button className="border-0 menu-btn m-1" icon={<GithubOutlined />} onClick={()=>window.open('https://github.com/enter1130')}><span style={window.innerWidth < 768 ?{display:'none'}:{}}>Github</span></Button>
             </Col>
             <Col>
-              <Button className="border-0 menu-btn m-1" icon={<MailOutlined />} onClick={()=>Copyable('chanzihang0311@gmail.com')} >chanzihang0311@gmail.com</Button>
+              <Button className="border-0 menu-btn m-1" icon={<MailOutlined />} onClick={()=>Copyable('chanzihang0311@gmail.com')} ><span style={window.innerWidth < 768 ?{display:'none'}:{}}>chanzihang0311@gmail.com</span></Button>
             </Col>
             <Col>
-              <Button className="border-0 menu-btn m-1" icon={<PhoneOutlined />} onClick={()=>Copyable('+886-900158238')} >+886-900158238</Button>
+              <Button className="border-0 menu-btn m-1" icon={<PhoneOutlined />} onClick={()=>Copyable('+886-900158238')} ><span style={window.innerWidth < 768 ?{display:'none'}:{}}>+886-900158238</span></Button>
             </Col>
             <Col>
-              <Button className="border-0 menu-btn m-1" icon={<DownloadOutlined />} onClick={()=>window.open('/resume.pdf')} >履歷 Download</Button>
+              <Button className="border-0 menu-btn m-1" icon={<DownloadOutlined />} onClick={()=>window.open('/resume.pdf')} ><span style={window.innerWidth < 768 ?{display:'none'}:{}}>履歷 Download</span></Button>
             </Col>
           </Row>
         </div>
